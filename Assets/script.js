f6eb53e30328888177c2386952036ac40a07d349
Reggie Littleton <Hitsupod@gmail.com>
2020-05-31 16:10:25 -0500

    Update script.js
    Initial Image API Creation

--- Assets/script.js
+++ Assets/script.js
@@ -10,48 +10,48 @@ var APIKey = "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088";
  "q=Bujumbura,Burundi&appid=" + APIKey;
 
 // Image API Info
-// var imgaeAPIKey = '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII';
+var imgaeAPIKey = '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII';
 // var imageSecretKey = 'pPLYR3b1xFOLPNeZVpb2G03orjzqA8kiMzUluQgBcqM';
 var citySearch = document.getElementById('city-input').nodeValue;
 var searchbtn = document.getElementById('add-city');
 
 // Background image function
-function initialrequest() {
+function initialRequest() {
     var imgURL = 'https://api.unsplash.com/search/photos/?query=' + citySearch;
-    var imgObj = new XMLHttpRequest();
-    imgObj.onreadystatechange = changeBackground;
-    imgObj.open('GET', imgURL, true);
-    imgObj.setRequestHeader('Authorization', '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII');
-    imgObj.send();
+    // var imgObj = new XMLHttpRequest();
+    // imgObj.onreadystatechange = changeBackground;
+    // imgObj.open('GET', imgURL, true);
+    // imgObj.setRequestHeader('Authorization', '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII');
+    //imgObj.send();
 
-    /*$.ajax({
+    $.ajax({
         url: 'https://api.unsplash.com/search/photos',
         dataType: 'json',
         method: 'GET',
         data:{
-            query: citySearch, 
-            client_id: imgaeAPIKey,
+            query: 'citySearch', 
+            client_id: '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII',
             page: 1,
             keyword: 'city',
         success: function(data){
-            console.log(data);   
+            return data;   
         },
         error: function(error){
             console.log(error);
             }
         }
-    })*/
+    })
 }
 
 function changeBackground(){
-    if(this.readyState == 4 && this.status === 200){
-        var response = JSON.parse(imgObj.responseText);
-        console.log(response);
-        var img = document.getElementById('backgroundImg');
-        
-        img.setAttributeNode(img) = response.results[0].user.profile_image.large;
-    }
+    //if(this.readyState == 4 && this.status === 200){
+        var response = JSON.parse(data.responseText);
+            console.log(response);
+            var imgResult = response.results[0].user.profile_image.large;
+            var img = document.getElementById('backgroundImg');
+            $('img').attr('src', imgResult);
+        // img.setAttributeNode(img) = result.results[0].user.profile_image.large; 
 }
 
-searchbtn.addEventListener('click', changeBackground);
+searchbtn.addEventListener('click', initialRequest);
 
