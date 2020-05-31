04e2e4e0c4b077499b3ff9d16eca5710d2972b9c
Reggie Littleton <Hitsupod@gmail.com>
2020-05-31 14:15:35 -0500

    Updated Image API

--- Assets/script.js
+++ Assets/script.js
@@ -10,26 +10,48 @@ var APIKey = "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088";
  "q=Bujumbura,Burundi&appid=" + APIKey;
 
 // Image API Info
-var imgaeAPIKey = '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII';
-var imageSecretKey = 'pPLYR3b1xFOLPNeZVpb2G03orjzqA8kiMzUluQgBcqM';
-var citySearch = $('#city-input').val();
-var imgURL = 'https://api.unsplash.com/search/photos';
+// var imgaeAPIKey = '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII';
+// var imageSecretKey = 'pPLYR3b1xFOLPNeZVpb2G03orjzqA8kiMzUluQgBcqM';
+var citySearch = document.getElementById('city-input').nodeValue;
+var searchbtn = document.getElementById('add-city');
 
 // Background image function
-function changeBackground() {
-    $.ajax({
-        url: imgURL,
+function initialrequest() {
+    var imgURL = 'https://api.unsplash.com/search/photos/?query=' + citySearch;
+    var imgObj = new XMLHttpRequest();
+    imgObj.onreadystatechange = changeBackground;
+    imgObj.open('GET', imgURL, true);
+    imgObj.setRequestHeader('Authorization', '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII');
+    imgObj.send();
+
+    /*$.ajax({
+        url: 'https://api.unsplash.com/search/photos',
         dataType: 'json',
         method: 'GET',
         data:{
             query: citySearch, 
             client_id: imgaeAPIKey,
-        },
+            page: 1,
+            keyword: 'city',
         success: function(data){
-            console.log(data);
+            console.log(data);   
+        },
+        error: function(error){
+            console.log(error);
+            }
         }
-    })
+    })*/
 }
-$('#add-city').on('click', function(event) {
-    
-})
+
+function changeBackground(){
+    if(this.readyState == 4 && this.status === 200){
+        var response = JSON.parse(imgObj.responseText);
+        console.log(response);
+        var img = document.getElementById('backgroundImg');
+        
+        img.setAttributeNode(img) = response.results[0].user.profile_image.large;
+    }
+}
+
+searchbtn.addEventListener('click', changeBackground);
+
