97e6bcc7ebf3d751f32387d33dbb13897d3c585e
mjaeschke <jaeschke21@gmail.com>
2020-06-01 18:19:18 -0500

    added the anime.js code to the travle dashbord and the outputs also added the weather api to the output but havent used the on click function for it yet

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
 var d = new Date();
 document.getElementById("currentTime").innerHTML = d;
-
-
+var apiKey = "&appid=c32cee6b53ed48e0afdd9368ee69e073";
+var apiToday = "https://api.openweathermap.org/data/2.5/weather?q=";
+var cityArr = ["San Antonio"];
+var todayUrl = apiToday+cityArr[0]+apiKey;
 // geolocation
 
 var APIKey = "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088";
@@ -12,9 +14,10 @@ var APIKey = "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088";
 // Image API Info
 var imgaeAPIKey = '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII';
 var imageSecretKey = 'pPLYR3b1xFOLPNeZVpb2G03orjzqA8kiMzUluQgBcqM';
-var citySearch = $('#city-input').val();
+var citySearch = document.getElementById('#city-input');
+//.val()
 var imgURL = 'https://api.unsplash.com/search/photos';
-
+changeBackground();
 // Background image function
 function changeBackground() {
     $.ajax({
@@ -30,6 +33,40 @@ function changeBackground() {
         }
     })
 }
+$.ajax({
+    url: todayUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var tempElm =$("#temp");
+    var temperature =$("<div>");
+    temperature.text("temp: "+response.main.temp);
+    tempElm.append(temperature);
+    temperature.text("temp: "+response.main.temp);
+    tempElm.append(temperature);
+    var humidityElm =$("#humidity");
+    var humidity =$("<div>");
+    humidity.text("humidity: "+response.main.humidity);
+    humidityElm.append(humidity);
+    var windElm =$("#windSpeed");
+    var windSpeed =$("<div>");
+    windSpeed.text("wind speed: "+response.wind.speed);
+    windElm.append(windSpeed);
+
+  });
+  anime({
+    targets: '#travl',
+    rotateY: 250,
+    direction: 'reverse'
+  });
+function rotate(){
+      anime({
+    targets: '#primaryP,#hotSpotP',
+    rotateY: 250,
+    direction: 'reverse'
+  });
+}
+
 $('#add-city').on('click', function(event) {
-    
+rotate();
 })
