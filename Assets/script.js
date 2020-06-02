bd9023756da1fcba37791975a326b8009a3bc7fc
rudy gauna <rudy.gauna@gmail.com>
2020-06-01 22:04:19 -0500

    added top airport wait times feature & additonal CSS stying

--- Assets/script.js
+++ Assets/script.js
@@ -11,6 +11,47 @@ var APIKey = "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088";
  var queryURL = "https://apility-io-ip-geolocation-v1.p.rapidapi.com/%7Bip%7D" +
  "q=Bujumbura,Burundi&appid=" + APIKey;
 
+<<<<<<< HEAD
+// airport wait times:
+
+var APIKey = "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088";
+
+document.getElementById('button').addEventListener('click', loadWait);
+
+
+// funcitn to load airport wait times
+    function loadWait(){
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://tsa-wait-times.p.rapidapi.com/airports/test?APIKEY=test");
+      xhr.setRequestHeader("x-rapidapi-host", "tsa-wait-times.p.rapidapi.com");
+      xhr.setRequestHeader("x-rapidapi-key", "33052e9e8amsh850a6e4503bd662p1ede3cjsn618b6bce7088");
+      xhr.onload = function(){
+        if(this.status == 200){
+          var wait = JSON.parse(this.responseText);
+
+          var output = '';
+          for(var i in wait){
+            output +=
+              '<div id="wait">' +
+              '<ul id="waitTimes">' +
+              '<li>Wait Time (hrs): '+wait[i].precheck+'</li>' +
+              '<li>City: '+wait[i].city+'</li>' +
+              '<li>Airport Code: '+wait[i].code+'</li>' +
+              '<li>City: '+wait[i].state+'</li>' +
+              '</ul>' +
+              '</div>';
+          }
+
+          document.getElementById("wait").innerHTML = output;
+        }
+      }
+
+      xhr.send();
+    }
+    xhr.send(data);
+
+        
+=======
 // Image API Info
 var imgaeAPIKey = '1XhY0EQp78t6XmGHtn59ng5GS1lh8DGX8IEyvFtWBII';
 var imageSecretKey = 'pPLYR3b1xFOLPNeZVpb2G03orjzqA8kiMzUluQgBcqM';
@@ -86,3 +127,4 @@ function rotate(){
 $('#add-city').on('click', function(event) {
 rotate();
 })
+>>>>>>> 4660073863352f23320b881d5e11e9a813e2deee
