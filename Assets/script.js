2b7e961a4289ada9fb67eb17182c24e9d7f4984a
mjaeschke <jaeschke21@gmail.com>
2020-06-02 17:58:02 -0500

    made the index work with the inout call

--- Assets/script.js
+++ Assets/script.js
@@ -53,8 +53,13 @@ $("#add-city").on("click", function (e) {
   e.preventDefault();
   rotate();
   weather();
-  $("#backgroundImg").empty();
+   $("#backgroundImg").empty();
+   $("#city").empty();
+   $("#temp").empty();
+   $("#humidity").empty();
+   $("#windSpeed").empty();
   var citySearch = $("#city-input").val();
+  
   var imgURL =
     "https://api.unsplash.com/search/photos?query=" +
     citySearch +
@@ -74,10 +79,10 @@ $("#add-city").on("click", function (e) {
 //   //////////////////////
 
 var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=";
-var cityName = document.getElementById("city-input").value;
+var cityName = "austin";
 var weatherApiKey = "&appid=c32cee6b53ed48e0afdd9368ee69e073";
 var units = "&units=imperial";
-var todayUrl = weatherApi + cityName + weatherApiKey;
+var todayUrl = weatherApi + cityName + weatherApiKey + units;
 
 function rotate() {
   anime({
@@ -94,20 +99,21 @@ function weather() {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    var cityElm = $("#city").empty();
+    var cityElm = $("#city");
     var cityNames = $("<h3>");
     cityNames.text("City Weather: " + response.name);
     cityElm.append(cityNames);
+
     var tempElm = $("#temp");
     var temperature = $("<div>");
     temperature.text("temp: " + response.main.temp);
     tempElm.append(temperature);
-    temperature.text("temp: " + response.main.temp);
-    tempElm.append(temperature);
+
     var humidityElm = $("#humidity");
     var humidity = $("<div>");
     humidity.text("humidity: " + response.main.humidity);
     humidityElm.append(humidity);
+
     var windElm = $("#windSpeed");
     var windSpeed = $("<div>");
     windSpeed.text("wind speed: " + response.wind.speed);
